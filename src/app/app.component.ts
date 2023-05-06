23422cc40b101fd9286b0c2f24a01083ff9614c3
sagadavid <epostsaga@gmail.com>
2023-05-06 12:48:14 +0200

    attribute binding on html, muliple attributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,47 +3,52 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name:string='david saga';
-  address:string='svale'
+  name: string = 'david saga';
+  address: string = 'svale';
   isOn = true;
-src2="https://images.unsplash.com/photo-1683220643085-1fa0ad87a1a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
-  textid = 'demoId';//a property now
+  isOff = false;
+  src2 =
+    'https://images.unsplash.com/photo-1683220643085-1fa0ad87a1a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80';
+  genislik = 500;
+
+  textid = 'demoId'; //a property now
 
   // myClasses = 'colorCode heighting';//string usage, value defined in css file
   // myClasses = ['colorCode','heighting'];//alternative usage
-  myClasses = {'colorCode':this.isOn,'heighting':this.feiler()};//selective usage of classes
+  myClasses = { colorCode: this.isOn, heighting: this.feiler() }; //selective usage of classes
 
-  getName(data:number):string{
-    if (data > 0)
-return 'address is '+ this.address+data;
-else {
-return 'no address to return';
-}}
-employees=['you', 'me', 'someone else'];
-employee={
-  title:'software developer', 
-  name:'saga',
-  src:'https://images.unsplash.com/photo-1682686579688-c2ba945eda0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  alt:'profile picture'
-}
+  getName(data: number): string {
+    if (data > 0) return 'address is ' + this.address + data;
+    else {
+      return 'no address to return';
+    }
+  }
+  employees = ['you', 'me', 'someone else'];
+  employee = {
+    title: 'software developer',
+    name: 'saga',
+    src: 'https://images.unsplash.com/photo-1682686579688-c2ba945eda0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    alt: 'profile picture',
+  };
 
-paaKlikkLagre():void{
-  alert('LagreKnappen er klikkket paa');
-  alert('navnet er oppdatert til ... ')
-  this.name ='... ingentin'
-}
+  paaKlikkLagre(): void {
+    alert('LagreKnappen er klikkket paa');
+    alert('navnet er oppdatert til ... ');
+    this.name = '... ingentin';
+  }
 
-overst():void{
-  this.name="det var david saga"
-}
+  overst(): void {
+    this.name = 'det var david saga';
+  }
 
-forandring():void{
-this.name="tekst forandret"
-}
+  forandring(): void {
+    this.name = 'tekst forandret';
+  }
 
-feiler():boolean{return false};
-
+  feiler(): boolean {
+    return false;
+  }
 }
